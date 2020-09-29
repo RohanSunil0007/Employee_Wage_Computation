2b8db0b34befbf22e72477ed47b8926f4b33e360
Rohan Sunil <reach.rohan0007@gmail.com>
2020-09-29 20:28:31 +0530

    Added : Daily Full Time Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,9 +2,16 @@
 echo "Welcome to Employee Wage Computation program"
 isPresent=1
 empCheck=$((1+RANDOM%2))
+attendance=0
 if [ $isPresent -eq $empCheck ]
 then
+	attendance=1
    echo "Employee is Present"
 else
+	attendance=0
    echo "Employee is Absent"
 fi
+empRatePerHour=20
+fullDayHour=8
+dailyEmpWage=$(( $empRatePerHour * $fullDayHour * $attendance ))
+echo "Daily Employee Wage : $dailyEmpWage"
