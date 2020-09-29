bd9baaa7348638fb7e60c18445378fc73ab970a4
Rohan Sunil <reach.rohan0007@gmail.com>
2020-09-29 23:48:05 +0530

    Added : Wages till total working hours or days reached for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,33 +1,32 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation program"
-empCheck=$((RANDOM%3))
 empRatePerHour=20
 
 isPartTime=1
 isFullTime=2
 
-fullDayHour=8
-partDayHour=4
 numWorkDays=20
 
-day=1
-while [ $day -le $numWorkDays ]
+MAX_WORKHRS_MONTH=100
+
+totalEmpHrs=0
+totalEmpWorkDays=0
+while [[ $totalEmpHrs -lt $MAX_WORKHRS_MONTH && $totalEmpWorkDays -lt $numWorkDays ]]
 do
+((totalEmpWorkDays++))
+empCheck=$((RANDOM%3))
 		case $empCheck in
 				$isPartTime)    echo "Employee is Present"
 						workHrs=4
-						echo "Part Time Salary is :"
 					;;
 				$isFullTime)	echo "Employee is Present"
 						workHrs=8
-						echo "Full Time Salary is :"
 					;;
 					*)
 						workHrs=0
 						echo "Employee is Absent !"
 					;;
 		esac
-	salary=$(( $empRatePerHour * $workHrs ))
-	totalsalary=$(($totalsalary+$salary))
-	((day++))
+	totalEmpHrs=$(($totalEmpHrs+$workHrs))
 done
+ salary=$(( $empRatePerHour * $totalEmpHrs ))
