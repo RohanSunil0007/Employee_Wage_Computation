37942235995d01517af8af7b1e36f134cde6994d
Rohan Sunil <reach.rohan0007@gmail.com>
2020-09-29 21:50:43 +0530

    Added: Updated using switch case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,20 +1,18 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation program"
-isPresent=1
 empCheck=$((1+RANDOM%2))
-attendance=0
-if [ $isPresent -eq $empCheck ]
-then
-	attendance=1
-   echo "Employee is Present"
-else
-	attendance=0
-   echo "Employee is Absent"
-fi
 empRatePerHour=20
 fullDayHour=8
 partDayHour=4
-FullTimedailyEmpWage=$(( $empRatePerHour * $fullDayHour * $attendance ))
+
+case $empCheck in
+					1) echo "Employee is Present"
+						FullTimedailyEmpWage=$(( $empRatePerHour * $fullDayHour  ))
+						PartTimedailyEmpWage=$(( $empRatePerHour * $partDayHour  ))
+					;;
+					2)
+						echo "Employee is Absent"
+					;;
+esac
 echo "Full Time Daily Employee Wage : $FullTimedailyEmpWage"
-PartTimedailyEmpWage=$(( $empRatePerHour * $partDayHour * $attendance ))
 echo "Part Time Daily Employee Wage : $PartTimedailyEmpWage"
